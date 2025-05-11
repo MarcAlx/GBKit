fe79ace3b6e4fc4e518c890afe9337094758161e
Marc-Alx <marc-alx@outlook.com>
2025-05-11 16:29:54 +0200

    [fix] ensure Timer behavior are closer to what they should be

--- Sources/GBKit/Components/Timer.swift
+++ Sources/GBKit/Components/Timer.swift
@@ -1,13 +1,24 @@
 /// maps Timer clock mode to its corresponding M cycle frequency
-public let TimerClockModeToFrequency:[Byte] = [
-    64, // 0b00
-    1,  // 0b01
-    4,  // 0b10
-    8   // 0b11
+let TimerClockModeToFrequency:[Int] = [
+    256, // 0b00
+    4,   // 0b01
+    16,  // 0b10
+    64   // 0b11
+]
+
+/// maps Timer clock mode to its corresponding M cycle frequency
+let TimerClockModeToFaillingEdgeBit:[ShortMask] = [
+    ShortMask.Bit_9, // 0b00
+    ShortMask.Bit_3, // 0b01
+    ShortMask.Bit_5, // 0b10
+    ShortMask.Bit_7  // 0b11
 ]
 
 ///ease timer related access
 public protocol TimerInterface {
+    ///16bit counter used to render DIV (not exposed on real game boy, yet used)
+    var INTERNAL_DIV_COUNTER:Short { get }
+    
     ///ease access to DIV
     var DIV:Byte { get }
     
@@ -25,6 +36,7 @@ public protocol TimerInterface {
 public class Timer : Component, Clockable {
     private let mmu:MMU
     private let interrupts:InterruptsControlInterface
+    private var overflowPending = false
     
     public init(mmu: MMU) {
         self.mmu = mmu
@@ -34,39 +46,47 @@ public class Timer : Component, Clockable {
     ///cycles this clock has elapsed
     public private(set) var cycles: Int = 0
     
+    ///clock mode extract from timer control TAC
+    private var clockMode:Int {
+        get {
+            return Int(self.mmu.TAC & 0b0000_0011 /*keep only first two bits*/)
+        }
+    }
+    
     /// perform a single tick on a clock, masterCycles and frameCycles  are provided for synchronisation purpose
     public func tick(_ masterCycles:Int, _ frameCycles:Int) -> Void {
-        //update div
-        if(masterCycles % GBConstants.DivTimerFrequency == 0){
-            let val:Byte = self.mmu.DIV &+ 1
-            self.mmu.directWrite(address: IOAddresses.DIV.rawValue, val: val)
+        let oldFallingEdgeBitSet = isBitSet(TimerClockModeToFaillingEdgeBit[self.clockMode], self.mmu.INTERNAL_DIV_COUNTER)
+        self.mmu.INTERNAL_DIV_COUNTER = self.mmu.INTERNAL_DIV_COUNTER &+ 4
+        let newFallingEdgeBitSet = isBitSet(TimerClockModeToFaillingEdgeBit[self.clockMode], self.mmu.INTERNAL_DIV_COUNTER)
+        
+        //n.b DIV will increment at 256 cycles as only 8 upper bits of INTERNAL_DIV_COUNTER are considered as DIV,
+        //    so it will take 256 INTERNAL_DIV_COUNTER tick to have DIV incremented by 1
+        
+        let tacEnabled:Bool = isBitSet(.Bit_2, self.mmu[IOAddresses.TAC.rawValue])
+        
+        
+        //we have wait 1 cycle during overflow -> trigger interrup
+        if(overflowPending){
+            //timer modulo
+            self.mmu.TIMA = self.mmu.TMA
+            //trigger interrupt
+            self.interrupts.setInterruptFlagValue(.Timer, true)
+            //reset
+            self.overflowPending = false
         }
         
-        //check if TAC is enable and corresponding frequency to increment TMA
-        if(isBitSet(.Bit_2, self.mmu[IOAddresses.TAC.rawValue])
-        && masterCycles % Int(TacClockFrequency) == 0){
-            let curTima:Byte = self.mmu.TIMA
-            let newTima:Byte = curTima &+ self.TacClockFrequency
-            //newTima is over cur value increments TIMA, otherwise (overflow) resets to the value stored at TMA
-            if(curTima<newTima){
-                self.mmu.TIMA = newTima
+        //tac enabled and edge bit has fallen (from 1 to 0)
+        if(tacEnabled && oldFallingEdgeBitSet && !newFallingEdgeBitSet) {
+            //TIMA will overflow by adding 1 -> reset TIMA with TMA and trigger interrupt
+            if(self.mmu.TIMA == 0xFF){
+                self.overflowPending = true
             }
             else {
-                //timer modulo
-                self.mmu.TIMA = self.mmu.TMA
-                //trigger interrupt
-                self.interrupts.setInterruptFlagValue(.Timer, true)
+                self.mmu.TIMA = self.mmu.TIMA &+ 1
             }
         }
         
         self.cycles = self.cycles &+ GBConstants.MCycleLength
-    }
-    
-    //Tac clock frequency in M-cycles as expressed by Tac mode
-    private var TacClockFrequency:Byte {
-        get {
-            return TimerClockModeToFrequency[Int(self.mmu.TAC & 0b0000_0011 /*keep only first two bits*/)]
-        }
     }
     
     public func reset(){
