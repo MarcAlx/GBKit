6a2435d4fbcf3c86d1346946c689c5a2896ba8a9
Marc-Alx <marc-alx@outlook.com>
2025-11-09 21:14:37 +0100

    [config] upgrade mac* version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "GBKit",
-    platforms: [.iOS(.v15),.macCatalyst(.v14), .macOS(.v11)],
+    platforms: [.iOS(.v15),.macCatalyst(.v14), .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
